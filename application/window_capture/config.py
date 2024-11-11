e2416b6668fd6c3209fb982344c397862841b5a7
Caio Nery <caionery@escavador.com>
2024-11-11 01:15:23 -0300

    feat: Adiciona path para pasta de placas recortadas

--- application/window_capture/config.py
+++ application/window_capture/config.py
@@ -4,4 +4,5 @@ from pathlib import Path
 
 DATA_FOLDER_PATH = Path(__file__).parents[1] / "data"
 FRAMES_FOLDER_PATH = DATA_FOLDER_PATH / "frames"
+PLATES_FOLDER_PATH = DATA_FOLDER_PATH / "plates"
 MODELS_FOLDER_PATH = Path(__file__).parents[1] / "models"
