5ffc1475e7c73b634e6afa0b734ce77064565fd8
Caio Nery <caionms@ufba.br>
2025-02-06 23:35:27 -0300

    feat: sem id no rtsp de placas

--- domain/rtsp_detection/plate_recognition.py
+++ domain/rtsp_detection/plate_recognition.py
@@ -38,7 +38,8 @@ from infrastructure.utils.plate_utils import (
     calculate_correct_plate,
     extract_and_save_cropped_images,
     format_license,
-    read_license_plate,
+    read_license_plate, experimental_add_or_update_ocr, experimental_read_license_plate,
+    experimental_extract_and_save_cropped_images,
 )
 from infrastructure.utils.plot_utils import plot_only_label
 
@@ -56,6 +57,7 @@ def main(
     image_folder_path: Path = FRAMES_FOLDER_PATH,
     type_of_camera: VehicleEnum = VehicleEnum.IN,
     camera_location: str = "Portaria 1 - Ondina",
+    qty_frames_before_detection: int = 3,
 ) -> None:
     """
     Captura continuamente a tela de uma janela específica ou da área de trabalho,
@@ -73,6 +75,7 @@ def main(
         image_folder_path (Path): O caminho da pasta onde as imagens anotadas serão salvas.
         type_of_camera (VehicleEnum): Indica se a câmera é uma entrada ou saída.
         camera_location (str): O local da câmera onde a detecção está sendo realizada.
+        qty_frames_before_detection (int): A quantidade de frames antes de realizar a detecção.
     """
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -136,9 +139,15 @@ def main(
 
     image_folder_path.mkdir(parents=True, exist_ok=True)
 
-    tracking_data: dict[int, dict[str, Any]] = TrackingData()
+    tracking_data: dict[str, Any] = {
+        "ocr_plates": [],
+        "registered": False,
+        "plate_type": None,
+        "final_plate": None,
+        "bbox": None,
+    }
 
-    last_run_time = time()
+    frames_detected = 0
 
     while True:
         loop_time = time()
@@ -152,95 +161,89 @@ def main(
             break
 
         # Run YOLOv8 inference on the frame
-        yaml_tracker = RESOURCES_FOLDER_PATH / "botsort.yaml"
-        results = list(
-            model.track(
-                source=frame, persist=True, stream=True, conf=0.87, tracker=str(yaml_tracker)
-            )
-        )
+        results = model(source=frame, conf=0.87)
 
         # Display the annotated frame
         annotated_frame = results[0].plot()
-        if show_video:
-            cv.imshow("Plate Detection Inference", annotated_frame)
 
-        # Recorta imagens das placas
-        cropped_images = extract_and_save_cropped_images(
-            img=frame, results=results, save_images=False
-        )
+        # Detectou uma placa
+        if len(results[0].boxes) > 0:
+            frames_detected += 1
 
-        # Placas OCR-izadas
-        for cropped_image in cropped_images:
-            ocr_plates = read_license_plate(cropped_image, ocr)
-            add_or_update_ocr(
-                tracking_data=tracking_data,
-                track_id=cropped_image.track_id,
-                ocrs=ocr_plates,
-                plate_type=cropped_image.plate_type,
-                registered=False,
+            # Recorta imagens das placas
+            cropped_images = experimental_extract_and_save_cropped_images(
+                img=frame, results=results, save_images=False
             )
 
-        # Verifica se passaram 10 segundos
-        if time() - last_run_time >= 10:
-            # Remove as placas já registradas
-            keys_to_remove = [
-                key for key, value in tracking_data.items() if value.get("registered", False)
-            ]
-            for key in keys_to_remove:
-                del tracking_data[key]
+            # Placas OCR-izadas
+            for cropped_image in cropped_images:
+                ocr_plates = experimental_read_license_plate(cropped_image, ocr)
+                experimental_add_or_update_ocr(
+                    tracking_data=tracking_data,
+                    ocrs=ocr_plates,
+                    plate_type=cropped_image.plate_type,
+                    bbox=cropped_image.bbox,
+                )
 
-            for track_id, track_data in tracking_data.items():
-                # Se não estiver registrado e houver mais de 10 OCRs, tenta registrar
-                if (
-                    not track_data["registered"]
-                    and track_data["ocr_plates"]
-                    and len(track_data["ocr_plates"]) > 6
-                ):
-                    formatted_plates = []
-                    for ocr_plate in track_data["ocr_plates"]:
-                        formatted_plate, success = format_license(
-                            ocr_plate, track_data["plate_type"]
-                        )
-                        if success:
-                            formatted_plates.append(formatted_plate)
-                    if formatted_plates:
-                        track_data["final_plate"] = calculate_correct_plate(formatted_plates)
+            if (
+                    frames_detected > qty_frames_before_detection
+                    and tracking_data["ocr_plates"]
+                    and len(tracking_data["ocr_plates"]) > 6
+            ):
+                formatted_plates = []
+                for ocr_plate in tracking_data["ocr_plates"]:
+                    formatted_plate, success = format_license(
+                        ocr_plate, tracking_data["plate_type"]
+                    )
+                    if success:
+                        formatted_plates.append(formatted_plate)
+                if formatted_plates:
+                    tracking_data["final_plate"] = calculate_correct_plate(formatted_plates)
 
-                        frame_path = (
-                            save_annotated_image(frame, str(image_folder_path))
-                            if track_data["final_plate"] not in validated_plates
-                            else None
-                        )
+                    frame_path = (
+                        save_annotated_image(frame, str(image_folder_path))
+                        if tracking_data["final_plate"] not in validated_plates
+                        else None
+                    )
 
-                        try:
+                    try:
+                        if tracking_data["final_plate"] not in validated_plates:
                             save_plate_results_to_json(
                                 file_path=str(output_json_path),
                                 type_of_camera=type_of_camera,
-                                plate_text=track_data["final_plate"],
-                                plate_type=track_data["plate_type"],
+                                plate_text=tracking_data["final_plate"],
+                                plate_type=tracking_data["plate_type"],
                                 camera_location=camera_location,
                                 frame_path=frame_path,
                             )
-                            track_data["registered"] = True
-                        except Exception:
-                            logger.exception("Error saving plate results to JSON.")
-                            track_data["registered"] = False
+                        tracking_data["registered"] = True
 
-            last_run_time = time()
+                        if (
+                                tracking_data["bbox"] is not None
+                                and tracking_data["final_plate"] is not None
+                        ):
+                            plot_only_label(
+                                img=annotated_frame,
+                                box=tracking_data["bbox"],
+                                text=tracking_data["final_plate"],
+                            )
 
-        if len(results[0].boxes) > 0 and any([box.id for box in results[0].boxes]):
-            for box, track_id, cls in zip(
-                results[0].boxes.xyxy.cpu(),
-                results[0].boxes.id.int().cpu().tolist(),
-                results[0].boxes.cls.int(),
-            ):
-                if (
-                    track_id in tracking_data.keys()
-                    and tracking_data[track_id]["final_plate"] is not None
-                ):
-                    plot_only_label(
-                        img=annotated_frame, box=box, text=tracking_data[track_id]["final_plate"]
-                    )
+                        tracking_data: dict[str, Any] = {
+                            "ocr_plates": [],
+                            "registered": False,
+                            "plate_type": None,
+                            "final_plate": None,
+                        }
+
+                    except Exception:
+                        logger.exception(
+                            "[PlateRecognition_VideoDetection] Error saving "
+                            "plate results to JSON."
+                        )
+                        tracking_data["registered"] = False
+
+        if show_video:
+            cv.imshow("Plate Recognition Inference", annotated_frame)
 
         # Debug da taxa de atualização
         logger.info(f"FPS: {1 / (time() - loop_time):.2f}")
@@ -258,4 +261,6 @@ if __name__ == "__main__":
     main(
         rtsp_url=rtsp_url,
         type_of_camera=VehicleEnum.IN,
+        show_video=True,
+        qty_frames_before_detection=3,
     )
