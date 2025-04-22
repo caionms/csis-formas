07d1c382757ef93f2e93f1259b467d90b3381af6
Caio Nery <caionms@ufba.br>
2025-04-21 23:06:48 -0300

    feat: deteccao com e sem tracking de placa via captura

--- domain/window_capture/plate_recognition.py
+++ domain/window_capture/plate_recognition.py
@@ -20,7 +20,9 @@ from config.paths import (
     DATA_FOLDER_PATH,
     FRAMES_FOLDER_PATH,
     MODELS_FOLDER_PATH,
+    RESOURCES_FOLDER_PATH,
 )
+from domain import TrackingData
 from domain.enums.plate_enum import VehicleEnum
 from infrastructure.logging.log_config import get_logger
 from infrastructure.utils.dashboard_utils import save_annotated_image, save_plate_results_to_json
@@ -32,11 +34,14 @@ from infrastructure.utils.model_utils import (
     initialize_yolo_model,
 )
 from infrastructure.utils.plate_utils import (
+    add_or_update_ocr,
     calculate_correct_plate,
     experimental_add_or_update_ocr,
     experimental_extract_and_save_cropped_images,
     experimental_read_license_plate,
+    extract_and_save_cropped_images,
     format_license,
+    read_license_plate,
 )
 from infrastructure.utils.plot_utils import plot_only_label
 from infrastructure.utils.window_capture_utils import capture_window, setup_capture_window
@@ -49,6 +54,194 @@ validated_plates = [
 
 
 def main(
+    window_title: str | None = None,
+    output_json_path: Path = DATA_FOLDER_PATH / "output_plates.json",
+    image_folder_path: Path = FRAMES_FOLDER_PATH,
+    type_of_camera: VehicleEnum = VehicleEnum.IN,
+    camera_location: str = "Portaria 1 - Ondina",
+) -> None:
+    """
+    Captura continuamente a tela de uma janela específica ou da área de trabalho,
+    realiza detecção de placas com o modelo do YOLO e em seguida faz o OCR com o
+    modelo do PaddleOCR e salva os resultados em um arquivo JSON.
+
+    A função exibe os frames anotados com as detecções em uma janela OpenCV e salva os
+    resultados de detecção e a imagem anotada a cada segundo, caso haja detecções.
+
+    Args:
+        window_title (Optional[str]): O título da janela a ser capturada. Se não for
+            especificado, captura a área de trabalho.
+        output_json_path (Path): O caminho do arquivo JSON onde os resultados das
+            detecções serão salvos.
+        image_folder_path (Path): O caminho da pasta onde as imagens anotadas serão salvas.
+        type_of_camera (VehicleEnum): Indica se a câmera é uma entrada ou saída.
+        camera_location (str): O local da câmera onde a detecção está sendo realizada.
+    """
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    # Prepara captura de janela
+    window_id = setup_capture_window(window_title)
+
+    # Load the plate detection model
+    plate_detection_model_filename = PLATE_YOLO_DETECTION_MODEL_DROPBOX_PATH.split("/")[-1]
+    plate_detection_model_path = MODELS_FOLDER_PATH / plate_detection_model_filename
+
+    try:
+        download_model(
+            plate_detection_model_path,
+            PLATE_YOLO_DETECTION_MODEL_DROPBOX_PATH,
+            DROPBOX_ACCESS_TOKEN,
+        )
+    except NoModelAvailableException as e:
+        logger.error(f"[PlateRecognition] {e} Detection cannot be performed.")
+        return
+
+    model = initialize_yolo_model(plate_detection_model_path)
+
+    # Load the OCR model
+    text_detection_model_filename = PLATE_PADDLE_DETECTION_MODEL_DROPBOX_PATH.split("/")[-1]
+    text_detection_model_path = MODELS_FOLDER_PATH / text_detection_model_filename
+
+    text_recognition_model_filename = PLATE_PADDLE_RECOGNITION_MODEL_DROPBOX_PATH.split("/")[-1]
+    text_recognition_model_path = MODELS_FOLDER_PATH / text_recognition_model_filename
+
+    text_cls_model_filename = PLATE_PADDLE_CLS_MODEL_DROPBOX_PATH.split("/")[-1]
+    text_cls_model_path = MODELS_FOLDER_PATH / text_cls_model_filename
+
+    try:
+        download_paddle_folder_model(
+            text_detection_model_path,
+            PLATE_PADDLE_DETECTION_MODEL_DROPBOX_PATH,
+            DROPBOX_ACCESS_TOKEN,
+        )
+        download_paddle_folder_model(
+            text_recognition_model_path,
+            PLATE_PADDLE_RECOGNITION_MODEL_DROPBOX_PATH,
+            DROPBOX_ACCESS_TOKEN,
+        )
+        download_paddle_folder_model(
+            text_cls_model_path, PLATE_PADDLE_CLS_MODEL_DROPBOX_PATH, DROPBOX_ACCESS_TOKEN
+        )
+    except NoModelAvailableException as e:
+        logger.error(f"[PlateRecognition] {e} Detection cannot be performed.")
+        return
+
+    ocr = initialize_paddleocr_model(
+        text_detection_model_path, text_recognition_model_path, text_cls_model_path
+    )
+
+    image_folder_path.mkdir(parents=True, exist_ok=True)
+
+    tracking_data: dict[int, dict[str, Any]] = TrackingData()
+
+    last_run_time = time()
+
+    while True:
+        loop_time = time()
+
+        screenshot = capture_window(window_id=window_id)
+
+        # Run YOLOv8 inference on the frame
+        yaml_tracker = RESOURCES_FOLDER_PATH / "botsort.yaml"
+        results = list(
+            model.track(
+                source=screenshot, persist=True, stream=True, conf=0.87, tracker=str(yaml_tracker)
+            )
+        )
+
+        # Display the annotated frame
+        annotated_frame = results[0].plot()
+        cv.imshow("Plate Detection Inference", annotated_frame)
+
+        # Recorta imagens das placas
+        cropped_images = extract_and_save_cropped_images(
+            img=screenshot, results=results, save_images=False
+        )
+
+        # Placas OCR-izadas
+        for cropped_image in cropped_images:
+            ocr_plates = read_license_plate(cropped_image, ocr)
+            add_or_update_ocr(
+                tracking_data=tracking_data,
+                track_id=cropped_image.track_id,
+                ocrs=ocr_plates,
+                plate_type=cropped_image.plate_type,
+                registered=False,
+            )
+
+        # Verifica se passaram 10 segundos
+        if time() - last_run_time >= 10:
+            # Remove as placas já registradas
+            keys_to_remove = [
+                key for key, value in tracking_data.items() if value.get("registered", False)
+            ]
+            for key in keys_to_remove:
+                del tracking_data[key]
+
+            for track_id, track_data in tracking_data.items():
+                # Se não estiver registrado e houver mais de 10 OCRs, tenta registrar
+                if (
+                    not track_data["registered"]
+                    and track_data["ocr_plates"]
+                    and len(track_data["ocr_plates"]) > 6
+                ):
+                    formatted_plates = []
+                    for ocr_plate in track_data["ocr_plates"]:
+                        formatted_plate, success = format_license(
+                            ocr_plate, track_data["plate_type"]
+                        )
+                        if success:
+                            formatted_plates.append(formatted_plate)
+                    if formatted_plates:
+                        track_data["final_plate"] = calculate_correct_plate(formatted_plates)
+
+                        frame_path = (
+                            save_annotated_image(screenshot, str(image_folder_path))
+                            if track_data["final_plate"] not in validated_plates
+                            else None
+                        )
+
+                        try:
+                            save_plate_results_to_json(
+                                file_path=str(output_json_path),
+                                type_of_camera=type_of_camera,
+                                plate_text=track_data["final_plate"],
+                                plate_type=track_data["plate_type"],
+                                camera_location=camera_location,
+                                frame_path=frame_path,
+                            )
+                            track_data["registered"] = True
+                        except Exception:
+                            logger.exception("Error saving plate results to JSON.")
+                            track_data["registered"] = False
+
+            last_run_time = time()
+
+        if len(results[0].boxes) > 0 and any([box.id for box in results[0].boxes]):
+            for box, track_id, cls in zip(
+                results[0].boxes.xyxy.cpu(),
+                results[0].boxes.id.int().cpu().tolist(),
+                results[0].boxes.cls.int(),
+            ):
+                if (
+                    track_id in tracking_data.keys()
+                    and tracking_data[track_id]["final_plate"] is not None
+                ):
+                    plot_only_label(
+                        img=annotated_frame, box=box, text=tracking_data[track_id]["final_plate"]
+                    )
+
+        # Debug da taxa de atualização
+        logger.info(f"FPS: {1 / (time() - loop_time):.2f}")
+
+        if cv.waitKey(1) == ord("q"):
+            cv.destroyAllWindows()
+            break
+
+    logger.info("[PlateDetection_WindowCaptureDetection] Done.")
+
+
+def main_without_tracking(
     window_title: str | None = None,
     output_json_path: Path = DATA_FOLDER_PATH / "output_plates.json",
     image_folder_path: Path = FRAMES_FOLDER_PATH,
@@ -234,11 +427,11 @@ def main(
             cv.destroyAllWindows()
             break
 
-    print("Done.")
+    logger.info("[PlateDetection_WindowCaptureDetection] Done.")
 
 
 if __name__ == "__main__":
-    main(
+    main_without_tracking(
         window_title="Reprodutor Multimídia",
         type_of_camera=VehicleEnum.OUT,
         qty_frames_before_detection=3,
